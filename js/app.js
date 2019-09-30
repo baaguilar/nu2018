664862619a184318bced38b847d4de029bc8e640
Brian Aguilar <baguilar@marketwatch.com>
2019-09-29 22:49:38 -0400

    tweaks to service workers

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// javascript
+if ('serviceWorker' in navigator) {
+    
+    navigator.serviceWorker
+        .register('sw.js', { scope: './'})
+        .then(function(registration){
+            console.log("Service Worker Registered", registration);
+    })
+    .catch(function(err){
+        console.log("Service Worked Failed to Register", err);
+    })
+}
